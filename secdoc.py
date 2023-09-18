a971d565e3d3abaca6b9cc814a082056f91404b8
Igor Trujnara <itrujnara@wp.pl>
2023-09-18 16:08:43 +0200

    Added "feature" type

--- secdoc.py
+++ secdoc.py
@@ -1,24 +1,43 @@
 from Bio.PDB.DSSP import dssp_dict_from_pdb_file
 import subprocess
 from itertools import groupby
-import sys
+
+class SecFeat():
+    def __init__(self, _type, _start, _end):
+        self.type = _type
+        self.start = _start
+        self.end = _end
+        self.length = self.end - self.start + 1
+
+    def __len__(self):
+        return self.length
 
 class SecStruc():
     def __init__(self, pdb_path, *, dssp_path="dssp", chain="A"):
         """
         Create a DSSP object from an input PDB and extract some useful variables.
         """
-        # get version of the DSSP executable
         dssp_ver = subprocess.run(["dssp", "--version"], capture_output=True, text=True).stdout.strip().split()[2]
         
-        # create a Biopython DSSP dict object
         self.dssp = dssp_dict_from_pdb_file(pdb_path, DSSP=dssp_path, dssp_version=dssp_ver)[0]
         
         # filter the entries in the dict and convert to a nicer format
         self.dssp = dict([(k[1][1], v[1]) for (k,v) in self.dssp.items() if k[0] == chain])
         
-        # extract the secondary structure into a string
         self.secseq = ''.join(self.dssp.values())
+        self.to_threestate()
+
+        self.features = self.make_features()
+
+    def make_features(self):
+        pos = 1
+        feats = []
+        for (c, group) in groupby(self.secseq):
+            L = sum([1 for _ in group])
+            feats.append(SecFeat(c, pos, pos + L - 1))
+            pos += L
+        return feats
+
     
     def threestate(self, *, helix="GHI", sheet="BE"):
         """
@@ -47,31 +66,29 @@ class SecStruc():
         Convert the structure string to run-length encoding. Pure.
         """
         out = ""
-        counts = ((x, sum(1 for _ in y)) for (x,y) in groupby(self.secseq))
-        for (x,y) in counts:
-            out += x
-            out += str(y)
+        for f in self.features:
+            out += f.type
+            out += str(len(f))
         return out
 
-    def featurelist(self):
+    def print_report(self):
         """
-        Create a list of pairs with secondary structures and their lengths. Pure.
+        Pretty-print a report of secondary structure to stdout.
         """
-        return list((x, sum(1 for _ in y)) for (x,y) in groupby(self.secseq))
+        features = self.features
+        labels = {'H': 'HELIX', 'E': 'SHEET', 'C': 'COIL'}
+        print("TYPE\tLENGTH\tSTART\tEND")
+        for feature in features:
+            print(f"{labels[feature.type]}\t{str(len(feature))}\t{str(feature.start)}\t{str(feature.end)}")
 
-    def featurereport(self, path=1):
+    def write_report(self, path):
         """
-        Pretty-print a report of secondary structure to stdout or a file.
+        Pretty-print a report of secondary structure to a file.
         """
-        features = self.featurelist()
+        features = self.features
+        labels = {'H': 'HELIX ', 'E': 'SHEET ', 'C': 'COIL  '}
         with open(path, 'w') as f:
             for feature in features:
-                if feature[0] == 'H':
-                    f.write("HELIX " + str(feature[1]) + '\n')
-                elif feature[0] == 'E':
-                    f.write("SHEET " + str(feature[1]) + '\n')
-                elif feature[0] == 'C':
-                    f.write("COIL  " + str(feature[1]) + '\n')
-
+                f.write(f"{labels[feature.type]}\t{str(len(feature))}\t{str(feature.start)}\t{str(feature.end)}\n")
 
 
