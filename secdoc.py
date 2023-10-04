704ae5940e1e789cf003a3236dabf737a8e9d999
Igor Trujnara <itrujnara@wp.pl>
2023-10-04 16:22:36 +0200

    Added shorthand and feature finding

--- secdoc.py
+++ secdoc.py
@@ -11,6 +11,10 @@ class SecFeat():
 
     def __len__(self):
         return self.length
+    
+    def __str__(self):
+        labels = {'H': 'HELIX', 'E': 'SHEET', 'C': 'COIL'}
+        return f"{labels[self.type]}\t{self.start}\t{self.end}\t{len(self)}"
 
 class SecStruc():
     def __init__(self, pdb_path, *, dssp_path="dssp", chain="A"):
@@ -37,7 +41,6 @@ class SecStruc():
             feats.append(SecFeat(c, pos, pos + L - 1))
             pos += L
         return feats
-
     
     def threestate(self, *, helix="GHI", sheet="BE"):
         """
@@ -61,34 +64,46 @@ class SecStruc():
         """
         self.secseq = self.threestate(helix=helix, sheet=sheet)
 
+    def shorthand(self):
+        """
+        Return a shorthand of secondary structure features, without lengths. Pure.
+        """
+        return ''.join(f.type for f in self.features)
+
     def runlength(self):
         """
         Convert the structure string to run-length encoding. Pure.
         """
-        out = ""
-        for f in self.features:
-            out += f.type
-            out += str(len(f))
-        return out
+        return ''.join(f.type + str(len(f)) for f in self.features)
 
     def print_report(self):
         """
         Pretty-print a report of secondary structure to stdout.
         """
-        features = self.features
         labels = {'H': 'HELIX', 'E': 'SHEET', 'C': 'COIL'}
         print("TYPE\tLENGTH\tSTART\tEND")
-        for feature in features:
+        for feature in self.features:
             print(f"{labels[feature.type]}\t{str(len(feature))}\t{str(feature.start)}\t{str(feature.end)}")
 
     def write_report(self, path):
         """
         Pretty-print a report of secondary structure to a file.
         """
-        features = self.features
-        labels = {'H': 'HELIX ', 'E': 'SHEET ', 'C': 'COIL  '}
+        labels = {'H': 'HELIX', 'E': 'SHEET', 'C': 'COIL'}
         with open(path, 'w') as f:
-            for feature in features:
+            for feature in self.features:
                 f.write(f"{labels[feature.type]}\t{str(len(feature))}\t{str(feature.start)}\t{str(feature.end)}\n")
+
+    def find_features(self, feature_type, feature_start = lambda x: True, feature_end = lambda x: True, feature_len = lambda x: True):
+        """
+        Find all features in a secondary structure that are of a certain type. Optionally, requirements on start position, end position or length can be added.
+        """
+        if not all([callable(feature_start), callable(feature_end), callable(feature_len)]):
+            raise TypeError("feature_start, feature_end and feature_len must be callable")
+        out_features = []
+        for feature in self.features:
+            if feature.type == feature_type and feature_start(feature.start) and feature_end(feature.end) and feature_len(len(feature)):
+                out_features.append(feature)
+        return out_features
 
 
