5177dccbeeae5b8168b66ab9f5c820c9c8d8548b
Igor Trujnara <itrujnara@wp.pl>
2023-11-09 15:34:50 +0100

    Fixed fetch script, tweaked plot script

--- scripts/from_af.py
+++ scripts/from_af.py
@@ -1,5 +1,6 @@
 import requests
 import tempfile
+import subprocess
 from secdoc import SecStruc
 
 def fetch_af_struc(crossref):
@@ -7,5 +8,8 @@ def fetch_af_struc(crossref):
     req = requests.get(url, allow_redirects = True)
     temp = tempfile.NamedTemporaryFile()
     temp.write(req.content)
-    s = SecStruc(temp.name)
+    res = subprocess.run(["grep", "-v", "DBREF", temp.name], capture_output = True)
+    temp2 = tempfile.NamedTemporaryFile()
+    temp2.write(res.stdout)
+    s = SecStruc(temp2.name)
     return s
